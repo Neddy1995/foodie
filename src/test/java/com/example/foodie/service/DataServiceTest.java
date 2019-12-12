860298592dc0c9bc0a1140e25828a789f7527141
Neddy1995 <xiechao.neddy@foxmail.com>
2019-12-12 17:05:08 +0800

    登录界面

--- src/test/java/com/example/foodie/service/DataServiceTest.java
+++ src/test/java/com/example/foodie/service/DataServiceTest.java
@@ -23,34 +23,62 @@ class DataServiceTest {
 
     @Test
     @DisplayName("测试DataService的查询当天各小时的点赞数selectLikeNumByDay方法")
-    void selectLikeNumByDay() {
-            List<DataBean> list = dataService.selectLikeNumByDay(new Date());
-            System.out.println(""+list.toString());
+    void selectLikeNumByDay() throws ParseException {
+        String s = "2019-11-25";
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+        Date date = sdf.parse(s);
+        List<DataBean> list = dataService.selectLikeNumByDay(date);
+        System.out.println(""+list.toString());
     }
 
     @Test
     @DisplayName("测试DataService的查询当天各小时的评论数selectCommentNumByDay方法")
-    void selectCommentNumByDay() {
+    void selectCommentNumByDay() throws ParseException {
+        String s = "2019-11-25";
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+        Date date = sdf.parse(s);
+        List<DataBean> list = dataService.selectCommentNumByDay(date);
+        System.out.println(""+list.toString());
     }
 
     @Test
     @DisplayName("测试DataService的查询当天各小时的关注数selectFollowNumByDay方法")
-    void selectFollowNumByDay() {
+    void selectFollowNumByDay() throws ParseException {
+        String s = "2019-11-25";
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+        Date date = sdf.parse(s);
+        List<DataBean> list = dataService.selectFollowNumByDay(date);
+        System.out.println(""+list.toString());
     }
 
     @Test
     @DisplayName("测试DataService的查询当天各小时的新建文章数selectArticleNumByDay方法")
-    void selectArticleNumByDay() {
+    void selectArticleNumByDay() throws ParseException {
+        String s = "2019-11-25";
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+        Date date = sdf.parse(s);
+        List<DataBean> list = dataService.selectArticleNumByDay(date);
+        System.out.println(""+list.toString());
     }
 
     @Test
     @DisplayName("测试DataService的查询当天各小时的审核通过文章数selectPassNumByDay方法")
-    void selectPassNumByDay() {
+    void selectPassNumByDay() throws ParseException {
+        String s = "2019-11-25";
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+        Date date = sdf.parse(s);
+        List<DataBean> list = dataService.selectPassNumByDay(date);
+        System.out.println(""+list.toString());
     }
 
     @Test
     @DisplayName("测试DataService的查询当天各小时的已审核数selectAuditNumByDay方法")
-    void selectAuditNumByDay() {
+    void selectAuditNumByDay() throws ParseException {
+        String s = "2019-11-25";
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+        Date date = sdf.parse(s);
+        List<DataBean> list = dataService.selectAuditNumByDay(date);
+        System.out.println(""+list.toString());
     }
 
     @Test
@@ -65,26 +93,51 @@ class DataServiceTest {
 
     @Test
     @DisplayName("测试DataService的查询当月各小时的关注数selectCommentNumByMonth方法")
-    void selectCommentNumByMonth() {
+    void selectCommentNumByMonth() throws ParseException {
+        String s = "2019-11";
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM");
+        Date date = sdf.parse(s);
+        List<DataBean> list = dataService.selectCommentNumByMonth(date);
+        System.out.println(""+list.toString());
     }
 
     @Test
     @DisplayName("测试DataService的查询当月各小时的评论数selectFollowNumByMonth方法")
-    void selectFollowNumByMonth() {
+    void selectFollowNumByMonth() throws ParseException {
+        String s = "2019-11";
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM");
+        Date date = sdf.parse(s);
+        List<DataBean> list = dataService.selectFollowNumByMonth(date);
+        System.out.println(""+list.toString());
     }
 
     @Test
     @DisplayName("测试DataService的查询当月各小时的新建文章数selectArticleNumByMonth方法")
-    void selectArticleNumByMonth() {
+    void selectArticleNumByMonth() throws ParseException {
+        String s = "2019-11";
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM");
+        Date date = sdf.parse(s);
+        List<DataBean> list = dataService.selectArticleNumByMonth(date);
+        System.out.println(""+list.toString());
     }
 
     @Test
     @DisplayName("测试DataService的查询当月各小时的通过审核的文章数selectPassNumByMonth方法")
-    void selectPassNumByMonth() {
+    void selectPassNumByMonth() throws ParseException {
+        String s = "2019-11";
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM");
+        Date date = sdf.parse(s);
+        List<DataBean> list = dataService.selectPassNumByMonth(date);
+        System.out.println(""+list.toString());
     }
 
     @Test
     @DisplayName("测试DataService的查询当月各小时的已审核文章数selectAuditNumByMonth方法")
-    void selectAuditNumByMonth() {
+    void selectAuditNumByMonth() throws ParseException {
+        String s = "2019-11";
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM");
+        Date date = sdf.parse(s);
+        List<DataBean> list = dataService.selectAuditNumByMonth(date);
+        System.out.println(""+list.toString());
     }
 }
