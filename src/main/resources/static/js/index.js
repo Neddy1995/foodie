0f4cbe98e1e73b89cb7e74cd03eecf392c0f0f50
Neddy1995 <xiechao.neddy@foxmail.com>
2020-01-03 17:05:30 +0800

    主页搜索功能跳转，添加多个页面

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
     //加载导航条
-    $('.mapTitle').load('./static/html/mapTitle.html');
+    $(".mapTitle").load("./static/html/mapTitle.html");
+
+
+    //搜索跳转，拼接路径
+    $(".search-btn").click(function () {
+        var text = $.trim($("#search-box").val());
+        console.log(text);
+        var url="list.html?text="+text;
+        window.open(encodeURI(url));
+    });
 });
